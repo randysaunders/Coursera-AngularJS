72f2f8367153ff0eeca4420cb9d5ff5b2c85fc3f
randysaunders <randy@saundersnet.com>
2017-03-05 09:12:41 -0500

    cosmetic changes

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,5 +1,5 @@
 (function () {
-	'use strict';
+'use strict';
 
 	angular.module('NarrowItDownApp', [])
 	.controller('NarrowItDownController', NarrowItDownController)
@@ -16,29 +16,25 @@
 				errorDisplay: '<'
 			},
 			controller: NarrowItDownController,
-            controllerAs: 'found',
-            bindToController: true
+			controllerAs: 'found',
+			bindToController: true
 		};
 
 		return ddo;
 	}
-	
+
 	NarrowItDownController.$inject = ['MenuSearchService'];
 	function NarrowItDownController(MenuSearchService) {
 		var narrowDown = this;
 		narrowDown.found = [];
-		narrowDown.emptySearch = "";
 		narrowDown.errorDisplay = false;
-		
+
 		narrowDown.search = function () {
-			console.log('searchTerm: ', narrowDown.searchTerm);
 
 			// Verify search term  exists
 			if (narrowDown.searchTerm == null || narrowDown.searchTerm === "") {
 				narrowDown.found = [];
 				narrowDown.errorDisplay = true;
-				narrowDown.emptySearch = "Nothing found";
-				console.log('empty outcome');
 				return;
 			}
 
@@ -51,21 +47,14 @@
 				if (response.length !== 0) {
 					narrowDown.errorDisplay = false;
 					narrowDown.found = response;
-					// console.log('good outcome');
-					// console.log('response: ', response);
-					narrowDown.emptySearch = "";
-
 				} else {
 					narrowDown.errorDisplay = true;
 					narrowDown.found = [];
-					// console.log('bad outcome');
-					// console.log('response: ', response);
-
 				}
 			})
 
 			.catch (function (error) {
-				console.log("Something went terribly wrong.");
+				console.error("Something went terribly wrong.");
 				narrowDown.errorDisplay = true;
 				narrowDown.found = [];
 			});
@@ -74,14 +63,6 @@
 		narrowDown.removeItem = function (itemIndex) {
 			narrowDown.found.splice(itemIndex, 1);
 		}
-		
-		narrowDown.noItemsFound = function () {
-			if (narrowDown.items.length === 0) { 
-				return true;
-			} else {
-				return false;
-			}	
-		}
 	}
 
 	MenuSearchService.$inject = ['$http', 'ApiBasePath'];
@@ -89,12 +70,6 @@
 		var service = this;
 
 		service.getMatchedMenuItems = function (searchTerm) {
-			// Does searchTerm exist?
-			// if (searchTerm == null || searchTerm === '') {
-			// 	return [];
-			// }
-
-			console.log('in service ...');
 
 			return $http({
 				method: "GET",
@@ -119,10 +94,9 @@
 					filteredItemList.push(unfilteredList[i]);
 				}
 			}
-			console.log('filteredItemList: ', filteredItemList);
 			return filteredItemList;
 		}
-		
+
 	}
 
 })();
