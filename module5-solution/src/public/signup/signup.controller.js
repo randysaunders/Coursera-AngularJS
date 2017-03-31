4e53661a9e4b7057f1ad9ee231aab2ffbdb95060
Saunders <randy.saunders@cincomsys.local>
2017-03-31 12:44:50 -0400

    fix favDish error

--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -4,8 +4,8 @@
 	angular.module('public')
 	.controller('SignUpController', SignUpController);
 
-	SignUpController.$inject = ['MenuService', 'RegistrationService'];
-	function SignUpController(MenuService, RegistrationService) {
+	SignUpController.$inject = ['MenuService', 'RegistrationService', '$scope'];
+	function SignUpController(MenuService, RegistrationService, $scope) {
 		var signUpCtrl = this;
 		signUpCtrl.userProfile = {};
 
@@ -16,16 +16,25 @@
 		}
 
 		signUpCtrl.validateFavorite = function () {
-			
-			signUpCtrl.userProfile.favorite = signUpCtrl.userProfile.favorite.toUpperCase();
-			
-			MenuService.getFavoriteDish(signUpCtrl.userProfile.favorite)
-			.then(function () {
-				signUpCtrl.favoriteValid = true;
-			})
-			.catch (function () {
+
+			if (signUpCtrl.userProfile.favorite) {
+				signUpCtrl.userProfile.favorite = signUpCtrl.userProfile.favorite.toUpperCase();
+
+				MenuService.getFavoriteDish(signUpCtrl.userProfile.favorite)
+				.then(function () {
+					signUpCtrl.favoriteValid = true;
+					$scope.regForm.favDish.$setValidity("favDish", true); // form element favDish is valid
+
+				})
+				.catch (function () {
+					signUpCtrl.favoriteValid = false;
+					$scope.regForm.favDish.$setValidity("favDish", false); // form element favDish is not valid
+				});
+			} else {
 				signUpCtrl.favoriteValid = false;
-			});
+				$scope.regForm.favDish.$setValidity("favDish", false); // form element favDish is not valid
+			}
+
 		}
 	};
 
